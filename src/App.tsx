bc689147b668f0f9f756158206fa5348588dd006
thffhh <sb6201035@gmail.com>
2026-06-08 09:04:12 +0600

    Create App component with initial layout

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <h1>🎉 সাইট সফলভাবে লাইভ হয়েছে!</h1>
+      <p>Vite + React + TypeScript</p>
+    </div>
+  )
+}
+
+export default App
